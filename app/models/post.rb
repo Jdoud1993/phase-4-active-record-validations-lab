e7be62ca9de4ca33e31f031467bcdcdca960408c
Joshua Doud <bigjj3393@yahoo.com>
2023-04-09 17:23:26 -0700

    commit tree 208bc8cc211c0591b364ba9af765da5fd978cdac

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,16 @@
 class Post < ApplicationRecord
+    validate :title_is_clickbait
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: {in: ["Fiction", "Non-Fiction"]}
+
+   
+
+    def title_is_clickbait
+        valids = [/Won't Believe/i, /Secret/i, /Top \d/i, /Guess/i]
+		if valids.none?{|par| par.match title}
+			errors.add(:title, "not clickbait")
+		end
+    end
 end
